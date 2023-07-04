bec03adbe43fa3a0223a9e15f6b403ffd01423ff
Daniil-Solo <daniil.solo1723@gmail.com>
2023-07-04 22:40:56 +0500

    remove redudant functions and calls

--- app.py
+++ app.py
@@ -1,89 +1,23 @@
-import bs4
-import requests
-import re
 import argparse
-from bs4 import BeautifulSoup
-from classes import *
+import datetime
+import time
+
+from parsing.async_parsing import get_programs
+from classes import ProgramView
 
 
-def get_text(element: bs4.Tag) -> str:
-    """
-    Возвращает текст элемента без табуляции, переноса кареток и новой строки
-    """
-    return element.text.replace("\n", "").replace("\t", "").replace("\r", "")
-
-
-def parse_programs() -> (list, list):
-    url = "https://epos.permkrai.ru/perm-itnetwork/directions/povyshenie-kvalifikaczii-dpo/"
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, features="html.parser")
-    links, titles = [], []
-    programs = soup.find("div", class_="promotes-block").children
-    programs = [p for p in programs if p.name]
+def main(view: str, only_actual: bool):
+    programs = get_programs()
     for program in programs:
-        link = program.a.get("href")
-        title = program.find("div", class_="title").text.strip()
-        links.append(link)
-        titles.append(title)
-    return links, titles
-
-
-def parse_courses(links: list, titles: list) -> list[Program]:
-    programs = []
-    for title, link in zip(titles, links):
-        response = requests.get(link)
-        soup = BeautifulSoup(response.text, features="html.parser")
-        program = Program(title, link)
-        div_courses = soup.find_all("div", class_="item-service")
-        for div_course in div_courses:
-            title = div_course.find("div", class_="name-service").text.strip()
-
-            opened_text = div_course.find_all("div", class_="text")
-            level = opened_text[0].find("span", class_="regular").text.strip()
-            study_format = opened_text[1].find("span", class_="regular").text.strip()
-            n_hours = opened_text[2].find("span", class_="regular").text.strip()
-            address = opened_text[3].find("span", class_="regular").text.strip()
-            is_actual = bool(div_course.find("a", class_="button-write"))
-
-            hidden_text = div_course.find("div", class_="hidden-block").div.p.text
-            dates = re.findall("\d{2}.\d{2}.\d{4}", hidden_text)
-            if not dates:
-                dates = re.findall("[\d]+ [\S]+ 2022", hidden_text)
-            try:
-                start_date = dates[0]
-                end_date = dates[1]
-            except IndexError:
-                start_date = end_date = ""
-            try:
-                start_index_uni = re.search("Вуз:", hidden_text).span()[1]
-                end_index_uni = start_index_uni + re.search("\n", hidden_text[start_index_uni:]).span()[0]
-                university = hidden_text[start_index_uni: end_index_uni].strip()
-            except AttributeError:
-                university = ""
-            schedule = get_text(div_course.find("div", class_="schedule-block").find_all("div")[1])
-
-            course = Course(is_actual, title, level, study_format, n_hours, address, start_date, end_date, university, schedule)
-            program.add_course(course)
-        programs.append(program)
-    return programs
-
-
-def main(view: str, url: bool, only_actual: bool):
-    links, titles = parse_programs()
-    programs = parse_courses(links, titles)
-    for program in programs:
-        if program.course_count(only_actual) > 0:
-            print(program.get_info(url))
-            print(program.get_info_about_courses(view, only_actual))
+        pw = str(ProgramView(program, view, only_actual))
+        print(pw)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--view", default="medium", type=str,
-                        help="type of view. small, medium, full")
-    parser.add_argument("--url", default=False, type=bool,
-                        help="outputs url of program")
-    parser.add_argument("--only_actual", default=True, type=bool,
-                        help="outputs only actual courses")
+    parser.add_argument("--view", default="small", type=str,
+                        help="type of view. small, full")
+    parser.add_argument("--only_actual", default="y", type=str,
+                        help="outputs only actual courses. y, n")
     args = parser.parse_args()
-    main(view=args.view, url=args.url, only_actual=args.only_actual)
+    main(view=args.view, only_actual=args.only_actual == "y")
