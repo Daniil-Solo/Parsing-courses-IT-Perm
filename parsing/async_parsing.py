c48043c53bbc30c305542dc4a32174ee146ae857
Daniil-Solo <daniil.solo1723@gmail.com>
2023-07-04 22:33:32 +0500

    add async parsing with aiohttp

---
+++ parsing/async_parsing.py
@@ -0,0 +1,27 @@
+import aiohttp
+import asyncio
+
+from classes import Program
+from parsing.data_extraction import get_link_for_programs, get_program_info
+from parsing.constants import ALL_PROGRAM_LINK
+
+
+async def get_document(link: str) -> str:
+    async with aiohttp.ClientSession() as session:
+        async with session.get(link) as resp:
+            return await resp.text()
+
+
+async def collect_program_documents():
+    home_document = await get_document(ALL_PROGRAM_LINK)
+    links = get_link_for_programs(home_document)
+    tasks = []
+    for link in links:
+        tasks.append(asyncio.create_task(get_document(link)))
+    documents = await asyncio.gather(*tasks)
+    return documents
+
+
+def get_programs() -> list[Program]:
+    documents = asyncio.run(collect_program_documents())
+    return [get_program_info(d) for d in documents]
---
+++ parsing/constants.py
@@ -0,0 +1,2 @@
+ALL_PROGRAM_LINK = "https://epos.permkrai.ru/perm-itnetwork/directions/napravleniya/zhitelyam-permskogo-kraya/" \
+                   "besplatnye-programmy-povysheniya-kvalifikaczii/"
