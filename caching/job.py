69a76cc207c32fae91cbb9568de3d0abec2c11c3
Daniil-Solo <daniil.solo1723@gmail.com>
2023-07-07 13:29:42 +0500

    add job for parsing and saving programs

---
+++ caching/job.py
@@ -0,0 +1,15 @@
+from classes import Program
+from parsing.async_parsing import get_programs
+from caching.redis_client import RedisClient
+
+
+def set_programs_in_cache(programs: [Program]):
+    with RedisClient() as cache:
+        cache.clear()
+        for program in programs:
+            cache.set_courses_by_program(program)
+
+
+if __name__ == "__main__":
+    all_programs = get_programs()
+    set_programs_in_cache(all_programs)
