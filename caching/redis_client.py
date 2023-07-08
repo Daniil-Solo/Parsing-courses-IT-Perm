9818effb8c63639fa56eeb95eebce9f1bfd3183c
Daniil-Solo <daniil.solo1723@gmail.com>
2023-07-08 23:47:53 +0500

    add async redis

--- caching/redis_client.py
+++ caching/redis_client.py
@@ -1,15 +1,15 @@
 from caching.cache import ProgramCache
 from config import REDIS_URL
-import redis
+import redis.asyncio as redis
 
 
 class RedisClient:
     def __init__(self):
         self.redis = None
 
-    def __enter__(self):
-        self.redis = redis.from_url(REDIS_URL)
+    async def __aenter__(self):
+        self.redis = await redis.from_url(REDIS_URL)
         return ProgramCache(self.redis)
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.redis.close()
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        await self.redis.close()
