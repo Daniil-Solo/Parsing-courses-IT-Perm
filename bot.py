8afc8951ce86778d8798a018be4978288717458b
Daniil-Solo <daniil.solo1723@gmail.com>
2023-07-11 16:13:56 +0500

    update with new version aiogram

--- bot.py
+++ bot.py
@@ -21,22 +21,18 @@ app = FastAPI()
 async def on_startup():
     webhook_info = await bot.get_webhook_info()
     if webhook_info.url != WEBHOOK_URL:
-        await bot.set_webhook(
-            url=WEBHOOK_URL
-        )
+        await bot.set_webhook(url=WEBHOOK_URL)
 
 
 @app.post(WEBHOOK_PATH)
 async def bot_webhook(update: dict):
     telegram_update = types.Update(**update)
-    Dispatcher.set_current(dp)
-    Bot.set_current(bot)
-    await dp.process_update(telegram_update)
+    await dp.feed_update(bot=bot, update=telegram_update)
 
 
 @app.on_event("shutdown")
 async def on_shutdown():
-    await bot.get_session().close()
+    await bot.session.close()
 
 
 @dp.message(Command(commands=["start"]))
